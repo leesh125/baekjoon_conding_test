f8bd38c892b9c38b13f9c6db05fec5e0dae9cbad
leesh125 <chelsea12345@naver.com>
2021-12-31 16:27:32 +0900

    단계별 풀기(그리디) - 동전 0

--- greedy/coin_0.py
+++ greedy/coin_0.py
@@ -1,11 +1,11 @@
-n, k = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
+n, k = map(int, input().split()) # 숫자 2개 입력받기
+arr = [int(input()) for _ in range(n)] # 동전 종류 입력받기
 cnt = 0
 
-for i in reversed(range(len(arr))):
-    if k // arr[i] != 0:
-        cnt += k // arr[i]
-        k %= arr[i]
+for i in reversed(range(len(arr))): # 역순으로 동전을 탐색
+    if k // arr[i] != 0:  # 해당 동전의 종류와 나눠떨어지면
+        cnt += k // arr[i] # 그 몫만큼 count 증가
+        k %= arr[i] # 초기 입력받은 금액을 나머지 금액으로 초기화
     else:
         continue
 

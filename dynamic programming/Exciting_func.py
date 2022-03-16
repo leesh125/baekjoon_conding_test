6b9bc08d2f34563a93b5bcd6bade2ce4068b506a
leesh125 <chelsea12345@naver.com>
2022-03-16 18:28:02 +0900

    단계별 풀기(동적 계획법 1) - 신나는 함수 실행

---
+++ dynamic programming/Exciting_func.py
@@ -0,0 +1,35 @@
+import sys
+
+d = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+
+
+def recursive(a, b, c):
+    global d
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return recursive(20, 20, 20)
+    if d[a][b][c] != 0:
+        return d[a][b][c]
+    if a < b and b < c:
+        d[a][b][c] = (
+            recursive(a, b, c - 1) + recursive(a, b - 1, c - 1) - recursive(a, b - 1, c)
+        )
+        return d[a][b][c]
+
+    d[a][b][c] = (
+        recursive(a - 1, b, c)
+        + recursive(a - 1, b - 1, c)
+        + recursive(a - 1, b, c - 1)
+        - recursive(a - 1, b - 1, c - 1)
+    )
+    return d[a][b][c]
+
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+
+    print(f"w({a}, {b}, {c}) = {recursive(a,b,c)}")

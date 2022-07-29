7630b5326bd8c72baf159a1a854512fb9ee27da3
leesh125 <chelsea12345@naver.com>
2022-07-30 02:43:12 +0900

    싸피 알고리즘 스터디(구현, 시뮬레이션) - BOJ_20291-파일 정리 설명 추가

--- temp/BOJ_20291.py
+++ temp/BOJ_20291.py
@@ -14,3 +14,15 @@ for _ in range(int(input())):
     dic[str] = 1 if str not in dic else dic[str] + 1
 for d in sorted(dic):
     print(d,dic[d])
+
+# Good Explanation
+import sys
+input = sys.stdin.readline
+def sol():
+    d=dict() # dictionary
+    n=int(input()) 
+    for line in range(n): # 읽어 들인 만큼
+        name,ext=input().strip().split('.') # split한 것 중 name 버리고 확장자명만 뽑음
+        d[ext]=d.get(ext,0)+1 # 없으면 새로 1, 있으면 기존값 +1
+    print('\n'.join(a+' '+str(d[a]) for a in sorted(d.keys()))) # 정렬된 것 조인해서 한줄 출력
+sol()

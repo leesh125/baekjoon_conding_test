1e2dce5e4c95679033c2ae4221237ff98d788536
leesh125 <chelsea12345@naver.com>
2022-08-05 14:50:11 +0900

    싸피 알고리즘 스터디(자료구조) - BOJ_9935-문자열 폭발(h)

--- 싸피 알고리즘 스터디/자료구조/BOJ_9935.py
+++ 싸피 알고리즘 스터디/자료구조/BOJ_9935.py
@@ -1,27 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-string = input()[:-1]
-bomb = input()[:-1]
-stack = []
-idx = 0
-len_string = len(string)
-len_bomb = len(bomb)
+string = input()[:-1] # 문자열
+bomb = input()[:-1] # 폭탄 문자열
+len_bomb = len(bomb) #폭탄 길이
+stack = [] # 스택
 
-stack.append(string[0])
+for s in string: # 문자 하나씩
+    stack.append(s) # 스택에 추가
+    # 현재 문자가 폭탄 문자열에 마지막 문자와 같고 이전값과 비교했을때 폭탄 문자열이라면
+    if s == bomb[-1] and bomb == ''.join(stack[-len_bomb:]):
+        for _ in range(len_bomb):
+            stack.pop() # 폭탄 문자열 만큼 빼내기
 
-for i in range(1,len_string):
-    if stack[-1] == bomb[idx]:
-        if idx == len_bomb-1:
-            for _ in range(len_bomb):
-                stack.pop()
-            continue
-        else:
-            idx += 1
-            stack.append(string[i])
-            
-    else:
-        idx = 0
-        stack.append(string[i])
-    print(stack, idx)
-# print(stack)
+print(''.join(stack) if stack else 'FRULA') # 출력
+
+# Good Explanation
+origin, to_remove,*a = open(0, 'rb').read().split(b'\n')
+L = len(to_remove)
+
+s = bytearray()
+for c in origin:
+    s.append(c)
+    if to_remove[-1] == c:
+        if to_remove == s[-L:]:
+            del s[-L:]
+print(s.decode() if s else 'FRULA')

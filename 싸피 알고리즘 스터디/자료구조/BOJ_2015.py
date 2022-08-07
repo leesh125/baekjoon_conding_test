2307e918601d1cfeddd0948d14c4c5908a9006dc
leesh125 <chelsea12345@naver.com>
2022-08-07 19:38:20 +0900

    싸피 알고리즘 스터디(자료구조) - BOJ_2015-수들의 합4(H) 풀이추가

--- 싸피 알고리즘 스터디/자료구조/BOJ_2015.py
+++ 싸피 알고리즘 스터디/자료구조/BOJ_2015.py
@@ -9,12 +9,28 @@ dp[0] = 0
 ans = 0
 
 for i in range(1,N+1):
-    dp[i] = nums[i-1] + dp[i-1]
+    dp[i] = nums[i-1] + dp[i-1] # 누적합
 
 dict = {}
-for i in range(N,-1,-1):
-    ans += dict.get(dp[i] + K,0)
-    dict[dp[i]] = dict.get(dp[i],0) + 1
+for i in range(N,-1,-1): # 거꾸로
+    ans += dict.get(dp[i] + K,0) # 현재 누적합 + K = 또다른 누적합 이 있으면 그 갯수만큼 +
+    dict[dp[i]] = dict.get(dp[i],0) + 1 # 현재 누적합 추가해주기
 print(ans)
 # B - A = K
 # B = A + K
+
+# Good Explanation
+import sys
+case = sys.stdin.readline().strip().split()
+N, K = int(case[0]), int(case[1])
+A = list(map(int, sys.stdin.readline().strip().split()))
+cnt, Sum, dic = 0, 0, {0: 1}
+for i in range(N):
+    Sum += A[i] # 누적합 (0~i번째 까지)
+    if Sum - K in dic: # Sum을 만들 수 있는 누적합이 존재한다면
+        cnt += dic[Sum - K] # 그 갯수만큼 cnt 더해줌
+    if Sum in dic: # 누적합이 있으면 그만큼 +=1
+        dic[Sum] += 1
+    else: # 없으면 새로 1
+        dic[Sum] = 1
+print(cnt)

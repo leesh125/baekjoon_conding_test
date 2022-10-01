aa03b34e2b0eb312d56034fbf30e60673580235e
leesh125 <chelsea12345@naver.com>
2022-10-01 18:12:29 +0900

    싸피 알고리즘 스터디(백트래킹) - BOJ_15663-N과 M (9)

---
+++ 싸피 알고리즘 스터디/백트래킹/Main_15663_N과M9.py
@@ -0,0 +1,38 @@
+from itertools import permutations
+
+n,m = map(int, input().split())
+nums = input().split()
+nums.sort(key=lambda x:int(x))
+ans = [0] * m
+visited = set()
+
+for p in permutations(nums, m):
+    s = ' '.join(p)
+    if s not in visited:
+        visited.add(s)
+        print(s)
+
+
+
+# def permutation(cnt):
+#     if cnt == m:
+#         str_ans = ''.join(map(str,ans))
+#         if str_ans not in dic:
+#             dic[str_ans] = True
+#             print(*ans)
+#         return
+    
+#     for i in range(n):
+#         if not visited[i]:
+#             visited[i] = True
+#             ans[cnt] = nums[i]
+#             permutation(cnt+1)
+#             visited[i] = False
+
+# n,m = map(int, input().split())
+# nums = sorted(list(map(int, input().split())))
+# ans = [0] * m
+# visited = [False] * n
+# dic = {}
+
+# permutation(0)

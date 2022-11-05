fd27ff2a51f3a3fc6d7d82a7fdd9f1b8a88ab979
leesh125 <chelsea12345@naver.com>
2022-11-06 02:04:51 +0900

    싸피 알고리즘 스터디(dp) - BOJ_11501-주식(h)

--- 싸피 알고리즘 스터디/그리디/Main_11501_주식.py
+++ 싸피 알고리즘 스터디/그리디/Main_11501_주식.py
@@ -1,19 +1,11 @@
 import sys
 input = sys.stdin.readline
-
 for _ in range(int(input())):
     N = int(input())
     nums = list(map(int,input().split()))
     ans = 0
-    dp = [0] * N
-
     max_num = nums[-1]
-    
     for i in range(N-2,-1,-1):
-        
-        if max_num > nums[i]:
-            dp[i] = max_num - nums[i]
-        else:
-            max_num = nums[i]
-        
-    print(sum(dp))
+        if max_num > nums[i]: ans += max_num - nums[i]
+        else:max_num = nums[i]
+    print(ans)

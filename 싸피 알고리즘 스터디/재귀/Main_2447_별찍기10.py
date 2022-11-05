2f0b6b9362288624b77ee7688e61a2ca4feb9316
leesh125 <chelsea12345@naver.com>
2022-11-05 18:31:22 +0900

    싸피 알고리즘 스터디(재귀) - BOJ_2447-별찍기10(h)

--- 싸피 알고리즘 스터디/재귀/Main_2447_별찍기10.py
+++ 싸피 알고리즘 스터디/재귀/Main_2447_별찍기10.py
@@ -1,7 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-pattern = '''***
-* *
-***'''
+def print_star(i,j,n):
+    if (i//n) % 3 == 1 and (j//n) % 3 == 1:
+        print(' ',end='')
+    else:
+        if n == 1:
+            print('*',end='')
+        else:
+            print_star(i,j,n//3)
 
+N = int(input())
+for i in range(N):
+    for j in range(N):
+        print_star(i,j,N)
+    print()
